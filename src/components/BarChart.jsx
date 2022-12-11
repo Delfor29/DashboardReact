ec40a5b3e1dd0b6b81f2aa4a712141052d4cc7db
Delfor Fuente <delforfuente@gmail.com>
2022-12-10 21:14:00 -0300

    GeographyChart ready.

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -2,7 +2,7 @@
 import { useTheme } from "@mui/material";
 import { ResponsiveBar } from "@nivo/bar";
 import { tokens } from "../theme";
-import {mockBardata as data} from "../data/mockData"
+import {mockBarData as data} from "../data/mockData"
 const BarChart = ( ) => {
 
 const theme = useTheme ();
