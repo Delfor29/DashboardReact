214a90e057ac37ac96d287c6123d9ef67f9a2e1b
Delfor Fuente <delforfuente@gmail.con>
2022-12-14 11:57:16 -0300

    Fix Line Chart

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,22 @@
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+// import { mockTransactions } from "../../data/mockData";
+// import  DownloadOutlinedIcon from "@mui/icons-material/DownloadDoneOutlined";
+// import  EmailIcon from "@mui/icons-material/Email";
+// import  PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+// import  PersonAddIcon from "@mui/icons-material/PersonAdd";
+// import  TrafficIcon from "@mui/icons-material/Traffic";
+// import LineChart from '../../components/LineChart';
+// import BarChart from '../../components/BarChart';
+// import GeographyChart from '../../components/GeographyChart';
+// import StatBox from '../../components/StatBox';
+// import ProgressCircle from '../../components/ProgressCircle';
 import Header from "../../components/Header";
-import { Box } from "@mui/material";
 
 const Dashboard = () => {
+    const theme = useTheme();
+    // const colors = tokens(theme.palette.mode);  //video 3:36:15
+
     return (
         <Box>
             <Box
--- src/scenes/line/index.jsx
+++ src/scenes/line/index.jsx
@@ -1,13 +1,13 @@
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 import Header from "../../components/Header";
 import LineChart from "../../components/LineChart";
 
 const Line =() => {
   return (
     <Box m="20px">
-      <Header title="Line Chart" subtitle= "Simple Pie Chart"/>
+      <Header title="Line Chart" subtitle= "Simple Pie Chart" />
       <Box height ="75vh">
-        <LineChart/>
+        <LineChart />
       </Box>
     </Box>
   );
